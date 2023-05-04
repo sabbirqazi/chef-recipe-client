9f6e30ae194f7afca972888f19755f43966c94e0
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-05-04 18:11:07 +0600

    blog added

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -4,48 +4,48 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RecipeDetails from "../RecipeDetails/RecipeDetails";
+import { HandThumbUpIcon } from "@heroicons/react/24/solid";
 
 const Recipe = ({ children }) => {
   const [banners, setBanners] = useState({});
   const { id } = useParams();
 
-
   useEffect(() => {
-    fetch(`https://chef-recipe-hunter-server-side-sabbirqazi.vercel.app/allData/${id}`)
+    fetch(
+      `https://chef-recipe-hunter-server-side-sabbirqazi.vercel.app/allData/${id}`
+    )
       .then((response) => response.json())
       .then((data) => setBanners(data));
   }, []);
 
-
   console.log(banners.items);
   return (
     <div>
-    
-      <div className="mx-28 my-10">
+
+      <div className="lg:mx-28 mx-5 my-10 flex flex-col justify-center items-center">
+        <div><h2 className="text-3xl font-semibold text-center mb-10">Welcome To View Recipes</h2></div>
         {banners?.items?.details?.map((banner) => (
-          <div
+        
+             <div
             key={banner?.id}
-            className="card card-side bg-base-100 shadow-xl"
+            className="card card-side bg-base-100 shadow-xl flex flex-col chef-banner-small lg:chef-banner-large"
           >
             <figure>
-              <img src={banner?.chef_picture_url} alt="Movie" />
+              <img  className="object-fit" src={banner?.chef_picture_url} alt="Chef" />
             </figure>
-            <div className="card-body">
-              <h2 className="card-title">{banner?.chef_name}</h2>
-              <p>{banner?.years_of_experience} years of experience</p>
-              <p>{banner?.number_of_recipes} recipes</p>
-              <p>{banner?.likes} likes</p>
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-semibold text-center">Name: {banner?.chef_name}</h2>
+              <p className="text-xl font-semibold">Experience: {banner?.years_of_experience} years of experience</p>
+              <p className="text-xl font-semibold">Recipes: {banner?.number_of_recipes} recipes</p>
+              <p className="text-xl font-semibold flex gap-2 items-center justify-center"> <span><HandThumbUpIcon className="h-8 w-8 text-amber-500" /></span> Likes: {banner?.likes}+</p>
             </div>
-          </div>
+          </div> 
         ))}
       </div>
 
-      <div className="mx-20 my-20 gap-10 grid grid-cols-1">
+      <div className="lg:mx-20 mx-5 my-20 gap-10 grid grid-cols-1">
         {banners?.items?.recipes?.map((recipe) => (
-          <RecipeDetails 
-          key={recipe?.id}
-          recipe ={recipe}
-          ></RecipeDetails>
+          <RecipeDetails key={recipe?.id} recipe={recipe}></RecipeDetails>
         ))}
       </div>
     </div>
--- src/pages/RecipeDetails/RecipeDetails.jsx
+++ src/pages/RecipeDetails/RecipeDetails.jsx
@@ -13,10 +13,10 @@ const RecipeDetails = ({recipe}) => {
           <Toaster />
       <div className="card bg-base-100 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title">{recipe?.name}</h2>
-          <p>Ingredients: {recipe?.ingredients}</p>
-          <p>Cooking Method: {recipe?.cooking_method}</p>
-          <p>Rating: {recipe?.rating}</p>
+          <h2 className="text-2xl font-bold">Dish: {recipe?.name}</h2>
+          <p className="text-xl font-semibold"><span className="text-2xl">Ingredients:</span> {recipe?.ingredients}</p>
+          <p className=" text-xl font-semibold"><span className="text-2xl">Cooking Method:</span>{recipe?.cooking_method}</p>
+          <p className=" text-xl font-semibold"><span className="text-2xl">Rating:</span> {recipe?.rating}</p>
           <div className="card-actions justify-end">
             <button className=" btn btn-primary" disabled={isFavorite} onClick={handleFavoriteClick}>
               {isFavorite ? "Favorited" : "Favorite"}
