903d435454f7d94a79a932e531cbd1cee651572c
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-08-08 20:59:51 +0600

    loginlayout changed

--- src/layouts/LoginLayOut.jsx
+++ src/layouts/LoginLayOut.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../pages/Shared/Navbar/Navbar';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const LoginLayOut = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
